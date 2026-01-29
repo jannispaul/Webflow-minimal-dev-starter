79ad2d0fbcdd7488f742195845a763dcf0e80de9
jannispaul <22460499+jannispaul@users.noreply.github.com>
2026-01-29 09:59:26 +0100

    updated from skypack to esm.sh

--- js/animations.js
+++ js/animations.js
@@ -17,8 +17,8 @@ gsap.registerPlugin(ScrollTrigger, SplitText); // List plugins here
 */
 
 // Import GSAP and ScrollTrigger
-import gsap from "https://cdn.skypack.dev/gsap";
-import { ScrollTrigger } from "https://cdn.skypack.dev/gsap/ScrollTrigger";
+import gsap from "https://esm.sh/gsap";
+import { ScrollTrigger } from "https://esm.sh/gsap/ScrollTrigger";
 
 // Ensure GSAP plugins are registered
 gsap.registerPlugin(ScrollTrigger);
